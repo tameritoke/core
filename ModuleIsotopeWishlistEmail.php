c88dc75d235c41fb2285c6fc64a13e0869991569
Yanick Witschi <yanick.witschi@certo-net.ch>
2011-10-07 14:40:34 +0000

    fixed incorrect links in back end wildcard

--- ModuleIsotopeWishlistEmail.php
+++ ModuleIsotopeWishlistEmail.php
@@ -58,7 +58,7 @@ class ModuleIsotopeWishlistEmail extends ModuleIsotope
 			$objTemplate->title = $this->headline;
 			$objTemplate->id = $this->id;
 			$objTemplate->link = $this->name;
-			$objTemplate->href = $this->Environment->script.'?do=modules&amp;act=edit&amp;id=' . $this->id;
+			$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
 
 			return $objTemplate->parse();
 		}
